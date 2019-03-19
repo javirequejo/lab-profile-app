5dafb29c3acdee4605d06c98c577c2df355696a9
Javier Requejo Melchor <javierrequejomelchor@gmail.com>
2019-03-19 13:28:03 +0100

    Some API corrections

--- profile-api/routes/auth.routes.js
+++ profile-api/routes/auth.routes.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../controllers/auth.controller');
 const uploader = require('../configs/storage.config');
+const secure = require('../middlewares/secure.mid');
 
 router.post('/register', auth.register);
 router.post('/authenticate', auth.authenticate);
-router.get('/profile', auth.getUser);
-router.put('/profile', uploader.single('attachment'), auth.updateUser);
+router.get('/profile', secure.isAuthenticated, auth.getUser);
+router.put('/profile', secure.isAuthenticated, uploader.single('attachment'), auth.updateUser);
 router.get('/logout', auth.logout);
 
 module.exports = router;
